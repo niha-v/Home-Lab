60bbb2f074bcd3846f3666c5e7b8bfd34c80fe13
Its Niharika Umrani <niharika.umrani@gmail.com>
2026-03-05 13:12:00 -0500

    Parse and filter Nessus vulnerability report
    This script reads a Nessus report, cleans the data by removing unnecessary rows and columns, and sorts the results by risk severity before saving to a new CSV file.

--- Nessus/Nessus_parser.py
+++ Nessus/Nessus_parser.py
@@ -1,1 +1,47 @@
+import pandas as pd
+
+file_path = "sample_nessus_report.csv"   
+df = pd.read_csv(file_path)
+
+# Clean column names (removes hidden spaces) 
+df.columns = df.columns.str.strip()
+
+df = df[df["Risk"].notna()]
+
+#Remove rows where Risk is "None" (case insensitive) 
+df = df[df["Risk"].str.strip().str.lower() != "none"]
+
+valid_risks = ["Critical", "High", "Medium", "Low"]
+df = df[df["Risk"].isin(valid_risks)]
+
+# Select important columns 
+columns_to_keep = [
+    "Host",
+     "Protocol",
+      "Port",
+    "Name",
+    "Risk",
+    "Severity",
+    "CVSS v3.0 Base Score"
+]
+
+existing_columns = [col for col in columns_to_keep if col in df.columns]
+df_filtered = df[existing_columns]
+
+# Sort by severity order 
+severity_order = ["Critical", "High", "Medium", "Low"]
+df_filtered["Risk"] = pd.Categorical(
+    df_filtered["Risk"],
+    categories=severity_order,
+    ordered=True
+)
+
+df_filtered = df_filtered.sort_values("Risk")
+
+# Save  file 
+df_filtered.to_csv("parsed_nessus.csv", index=False)
+
+# Display results
+print("\nParsed Nessus Vulnerability Report:\n")
+print(df_filtered.head(20))
 
